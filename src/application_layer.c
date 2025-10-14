2faa339a7bbd70abbc26795872bddddd59959b26
Diogo Costa <up202108655@up.pt>
2025-10-14 14:45:39 +0100

    application layer

--- src/application_layer.c
+++ src/application_layer.c
@@ -1,11 +1,28 @@
 // Application layer protocol implementation
 
 #include "application_layer.h"
+#include "link_layer.h"
+#include <string.h>
 
 #include <stdio.h>
 
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
-    // TODO: Implement this function
+
+    LinkLayer connectionParameters;
+    connectionParameters.baudRate = baudRate;
+    connectionParameters.nRetransmissions = nTries;
+    strcpy(connectionParameters.role, role);
+    strcpy(connectionParameters.serialPort, serialPort);
+    connectionParameters.timeout = timeout;
+
+    printf("Testing llopen\n");
+    if (llopen(connectionParameters) == -1) {
+        printf("Error occured\n");
+        return;
+    }
+
+    printf("\nEverything right\n");
+    return;
 }
